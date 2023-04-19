6f4e7c6777c30257988fed82668cd3a5d4b7d82f
flub78 <frederic.peignot>
2023-04-19 13:13:10 +0200

    Test multiple terrains CRUD

--- tests/Browser/TerrainTest.php
+++ tests/Browser/TerrainTest.php
@@ -7,63 +7,81 @@ use Tests\GvvDuskTestCase;
 
 class TerrainTest extends GvvDuskTestCase {
 
-    public function createTerrain($browser) {
+    /**
+     * Test create elements
+     */
+    public function createTerrain($browser, $terrains = []) {
 
-        $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
-        $total = $this->TableTotal($browser);
+        foreach ($terrains as $terrain) {
+            $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
+            $total = $this->TableTotal($browser);
 
-        $this->canAccess($browser, "terrains/create", ['Planeur']);
+            $this->canAccess($browser, "terrains/create", ['Code OACI']);
 
-        $glider = ['mpconstruc' => 'Test Glider', 'type' => 'planeur', 'seats' => 1, 'weight' => 100, 'max_weight' => 100, 'max_fuel' => 100, 'max_fuel_type' => 'L', 'max_fuel_weight' => 100, 'max_fuel_weight_type' => 'L'];
+            $browser
+                ->type('oaci', $terrain['oaci'])
+                ->type('nom', $terrain['nom'])
+                ->type('freq1', $terrain['freq1'])
+                ->type('comment', $terrain['comment'])
+                ->press('#validate')
+                ->assertSee('Terrains');
 
-        $browser
-            ->type('oaci', "LFAA")
-            ->type('nom', "Trifouillis")
-            ->type('freq1', "123.45")
-            ->type('comment', "Mon terrain")
-            ->press('#validate')
-            ->assertSee('Terrains');
+            $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
+            $new_total = $this->TableTotal($browser);
 
-        $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
-        $new_total = $this->TableTotal($browser);
-
-        $this->assertEquals($total + 1, $new_total, "Terrain created, total = " . $new_total);
+            $this->assertEquals($total + 1, $new_total, "Terrain created, total = " . $new_total);
+        }
     }
 
-    public function deleteTerrain($browser) {
+    /**
+     * Test delete elements, GVV just ignore the command when elements are selected several times
+     * 
+     * @depends testTerrainCRUD
+     * @param Browser $browser
+     * @param array $terrains
+     */
+    public function deleteTerrain($browser, $terrains = []) {
 
-        $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
-        $total = $this->TableTotal($browser);
+        foreach ($terrains as $terrain) {
+            $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
+            $total = $this->TableTotal($browser);
 
-        $this->canAccess($browser, "terrains/delete/" . "LFAA", ['Terrains']);
-        $new_total = $this->TableTotal($browser);
+            $this->canAccess($browser, "terrains/delete/" . $terrain['oaci'], ['Terrains']);
+            $new_total = $this->TableTotal($browser);
 
-        $this->assertEquals($total -1, $new_total, "Terrain deleted, total = " . $new_total);
+            $this->assertEquals($total - 1, $new_total, "Terrain deleted, total = " . $new_total);
+        }
     }
 
-    public function createTerrainError($browser) {
+    /**
+     * Test create elements that already exist
+     */
+    public function createTerrainError($browser, $terrains = []) {
 
-        $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
-        $total = $this->TableTotal($browser);
+        foreach ($terrains as $terrain) {
+            $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
+            $total = $this->TableTotal($browser);
 
-        $this->canAccess($browser, "terrains/create", ['Planeur']);
+            $this->canAccess($browser, "terrains/create", ['Planeur']);
 
-        $glider = ['mpconstruc' => 'Test Glider', 'type' => 'planeur', 'seats' => 1, 'weight' => 100, 'max_weight' => 100, 'max_fuel' => 100, 'max_fuel_type' => 'L', 'max_fuel_weight' => 100, 'max_fuel_weight_type' => 'L'];
+            $browser
+                ->type('oaci', $terrain['oaci'])
+                ->type('nom', $terrain['nom'])
+                ->type('freq1', $terrain['freq1'])
+                ->type('comment', $terrain['comment'])
+                ->press('#validate')
+                ->assertSee("L'élément existe déjà");
 
-        $browser
-            ->type('oaci', "LFAA")
-            ->type('nom', "Trifouillis")
-            ->type('freq1', "123.45")
-            ->type('comment', "Mon terrain")
-            ->press('#validate')
-            ->assertSee("L'élément existe déjà");
+            $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
+            $new_total = $this->TableTotal($browser);
 
-        $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
-        $new_total = $this->TableTotal($browser);
-
-        $this->assertEquals($total, $new_total, "No Terrain created, total = " . $new_total);
+            $this->assertEquals($total, $new_total, "No Terrain created, total = " . $new_total);
+        }
     }
 
+    /**
+     * Test Creation, Read, Update and Delete of a Terrain
+     */
     public function testTerrainCRUD() {
         $this->browse(function (Browser $browser) {
 
@@ -71,12 +89,17 @@ class TerrainTest extends GvvDuskTestCase {
 
             $this->canAccess($browser, "terrains/page", ['Compta', 'Terrains']);
 
+            $terrains = [
+                ['oaci' => "LFAA", 'nom' => "Trifouillis", 'freq1' => "123.45", 'comment' => "Mon terrain"],
+                ['oaci' => "LFAB", 'nom' => "Les Oies", 'freq1' => "123.45", 'comment' => "Mon second terrain"]
+            ];
+
             $total = $this->TableTotal($browser);
             $this->assertGreaterThan(0, $total, "Terrains Table contains some entries");
 
-            $this->createTerrain($browser);
-            $this->createTerrainError($browser);
-            $this->deleteTerrain($browser);
+            $this->createTerrain($browser, $terrains);
+            $this->createTerrainError($browser, $terrains);
+            $this->deleteTerrain($browser, $terrains);
 
             $this->logout($browser);
         });
