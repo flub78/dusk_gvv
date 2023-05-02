8396177c26c6bf082718f159e3b3357620980d37
flub78 <frederic.peignot>
2023-05-02 09:27:11 +0200

    Test that test methods are run sequentially

---
+++ doc/gvv_test_list.md
@@ -0,0 +1,31 @@
+# GVV test list
+
+## Existing tests
+
+### Installation test
+
+Check the installation page, the reset and the database migration
+
+### Page access
+
+This tests checks that the pages can be accessed or not according to the user role.
+
+* Admin
+* Bureau
+* CA
+* Planchiste
+* User
+
+* Accounter (TBD)
+
+### CI Unit tests
+
+Run the CI Unit tests and checks that they do not report errors.
+
+## Tests to develop
+
+* Smoke test
+* Backup/Restore test
+* Email and forgotten password
+* CRUD for all resources
+
--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -5,19 +5,56 @@ namespace Tests\Browser;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class ExampleTest extends DuskTestCase
-{
+/**
+ * Class ExampleTest
+ * 
+ * Check that the environment is working
+ * 
+ * php artisan dusk --color=always --browse tests/Browser/ExampleTest.php
+ * 
+ * @package Tests\Browser
+ */
+class ExampleTest extends DuskTestCase {
+
+
     /**
      * A basic browser test example.
      *
      * @return void
      */
-    public function testBasicExample()
-    {
+    public function testBasicExample() {
+        global $cnt;
         $this->browse(function (Browser $browser) {
             $browser->visit('http://aeroclub-abbeville.fr/')
-                    ->assertSee('Buigny');
+                ->assertSee('Buigny');
             $browser->screenshot('Aeroclub');
+        });
+    }
+
+    /**
+     * A test to check access to the Google search page.
+     *
+     * @return void
+     */
+    public function testGoogleSearch() {
+        global $cnt;
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('https://www.google.com/')
+                ->assertSee('Google');
+            $browser->screenshot('Google');
+        });
+    }
+
+    /**
+     * A test to check access to GVV on Oracle Cloud.
+     */
+    public function testGvv() {
+        global $cnt;
+        $this->browse(function (Browser $browser) {
+            $browser->visit('https://gvv-live.flub78.net/index.php/')
+                ->assertSee('GVV');
+            $browser->screenshot('GVV');
         });
     }
 }
