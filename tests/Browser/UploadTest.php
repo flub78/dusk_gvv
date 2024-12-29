fd4d92e0b99b95b9afd5ac8d2825f2a4f790640c
Flub78 <frederic.peignot@free.fr>
2024-12-29 16:57:59 +0100

    Test that the  uploaded files are actually saved on the server

--- tests/Browser/UploadTest.php
+++ tests/Browser/UploadTest.php
@@ -6,9 +6,9 @@ use Laravel\Dusk\Browser;
 use Tests\GvvDuskTestCase;
 
 /**
- * Test Terrain CRUD
+ * Test upload image
  * 
- * php artisan dusk --color=always --browse tests/Browser/TerrainTest.php
+ * php artisan dusk --color=always --browse tests/Browser/UploadTest.php
  * 
  * The tests rely on the method order. 
  */
@@ -17,12 +17,6 @@ class UploadTest extends GvvDuskTestCase {
     /** Constructor */
     function __construct() {
         parent::__construct();
-
-        // var_dump($this->terrains);
-        $this->terrains = [
-            ['oaci' => "LFAA", 'nom' => "Trifouillis", 'freq1' => "123.45", 'comment' => "Mon terrain"],
-            ['oaci' => "LFAB", 'nom' => "Les Oies", 'freq1' => "123.45", 'comment' => "Mon second terrain"]
-        ];
     }
 
     // protected function setUp(): void {
@@ -59,6 +53,12 @@ class UploadTest extends GvvDuskTestCase {
         // $this->markTestSkipped('must be revisited.');
         $this->browse(function (Browser $browser) {
 
+            $upload_dir = "/var/www/html/gvv.net/uploads/";
+            $initial_count = 0;
+            if (is_dir($upload_dir)) {
+                $initial_count = count(glob($upload_dir . '*'));
+            }
+
             // Check that the test image exists
             $image_path = base_path('tests/fixtures/images/asterix.jpeg');
             $this->assertFileExists($image_path);
@@ -72,6 +72,11 @@ class UploadTest extends GvvDuskTestCase {
 
             // Check that an error has been reported
             $browser->assertSee('Vous n\'avez pas de fiche personnelle');
+
+            if (is_dir($upload_dir)) {
+                $final_count = count(glob($upload_dir . '*'));
+                $this->assertEquals($initial_count, $final_count, "no file left in case of error");
+            }
         });
     }
 
@@ -82,6 +87,14 @@ class UploadTest extends GvvDuskTestCase {
     public function testUploadOnEdit() {
         // $this->markTestSkipped('must be revisited.');
         $this->browse(function (Browser $browser) {
+
+            $upload_dir = "/var/www/html/gvv.net/uploads/";
+            $initial_count = 0;
+
+            // if the upload directory exists, count the number of files in it
+            if (is_dir($upload_dir)) {
+                $initial_count = count(glob($upload_dir . '*'));
+            }
 
             $image_path = base_path('tests/fixtures/images/asterix.jpeg');
             $this->assertFileExists($image_path);
@@ -98,9 +111,15 @@ class UploadTest extends GvvDuskTestCase {
                 $browser->assertPresent('#delete_photo');
 
                 $browser->press('#delete_photo');
+
+                if (is_dir($upload_dir)) {
+                    $new_count = count(glob($upload_dir . '*'));
+                    $this->assertEquals($initial_count - 1, $new_count);
+                }
             } else {
                 // There is no photo
                 $browser->assertNotPresent('#delete_photo');
+                $new_count = $initial_count;
             }
 
             // Upload the image
@@ -114,6 +133,11 @@ class UploadTest extends GvvDuskTestCase {
             $browser->assertVisible('#photo');
             $browser->assertVisible('#delete_photo');
 
+            if (is_dir($upload_dir)) {
+                $count_after_upload = count(glob($upload_dir . '*'));
+                $this->assertEquals($new_count + 1, $count_after_upload);
+            }
+
             // if there was no prexisting photo
             if (!$existing_photo) {
                 // delete the photo that has just been uploaded
@@ -121,6 +145,16 @@ class UploadTest extends GvvDuskTestCase {
 
                 $browser->assertNotPresent('#photo');
                 $browser->assertNotPresent('#delete_photo');
+
+                if (is_dir($upload_dir)) {
+                    $count_after_delete = count(glob($upload_dir . '*'));
+                    $this->assertEquals($count_after_upload - 1, $count_after_delete);
+                }
+            }
+
+            if (is_dir($upload_dir)) {
+                $final_count = count(glob($upload_dir . '*'));
+                $this->assertEquals($final_count, $initial_count);
             }
         });
     }
